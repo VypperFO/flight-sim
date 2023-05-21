db7cadee5f80bf30554b587956c8d7ef7b1fc8e3
YourStepSis <charlesolivierlefebvre99@gmail.com>
2023-05-20 20:04:56 -0400

    c

--- Tp-02-02/Tp-02-02/view/FormSimulator.Designer.cs
+++ Tp-02-02/Tp-02-02/view/FormSimulator.Designer.cs
@@ -28,12 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            label1 = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            label3 = new Label();
+            listBox2 = new ListBox();
+            label5 = new Label();
+            listBox3 = new ListBox();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = Properties.Resources.Fortnite_Map_Season_1__2_;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Location = new Point(14, 228);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(600, 600);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Simulation";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "XML files (*.XML)|*.XML|All files (*.*)|*.*";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(14, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(456, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Selectionner fichier";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(487, 27);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Démarrer";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(14, 91);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 124);
+            listBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(14, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Aéroport";
+            label2.Click += label2_Click_1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(152, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Client";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(152, 91);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(318, 124);
+            listBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(487, 73);
+            label5.Name = "label5";
+            label5.Size = new Size(44, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Avions";
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(487, 91);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(127, 124);
+            listBox3.TabIndex = 8;
+            // 
+            // FormSimulator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(626, 840);
+            Controls.Add(label5);
+            Controls.Add(listBox3);
+            Controls.Add(label3);
+            Controls.Add(listBox2);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "FormSimulator";
+            Text = "Simulateur de scénario";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private Label label2;
+        private Label label3;
+        private ListBox listBox2;
+        private Label label5;
+        private ListBox listBox3;
     }
 }
--- Tp-02-02/Tp-02-02/view/FormSimulator.cs
+++ Tp-02-02/Tp-02-02/view/FormSimulator.cs
@@ -5,6 +5,34 @@ namespace Tp_02_02
         public FormSimulator()
         {
             InitializeComponent();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            button2.Enabled = false;
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
